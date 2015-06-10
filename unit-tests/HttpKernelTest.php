2395b16dd305197b2853c9bb8f4451cf702b1909
Simon Vocella <voxsim@Simons-MacBook-Pro.local>
2015-06-10 13:02:59 +0200

    added tests for HttpKernel

--- unit-tests/HttpKernelTest.php
+++ unit-tests/HttpKernelTest.php
@@ -2,8 +2,10 @@
 
 use Minima\HttpKernel;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class HttpKernelTest extends \PHPUnit_Framework_TestCase {
 
@@ -38,6 +40,91 @@ class HttpKernelTest extends \PHPUnit_Framework_TestCase {
     $this->requestStack->expects($this->once())->method('pop');
 
     $this->httpKernel->handle($this->request);
+  }
+
+  public function testHandleAfterFirstRequestEvent() {
+    $this->requestStack->expects($this->once())->method('push');
+
+    $this->dispatcher->expects($this->at(0))->method('dispatch')->with(KernelEvents::REQUEST, $this->anything())
+	 ->willReturnCallback(function($type, $event) {
+		$event->setResponse(new Response(''));
+	 });
+    $this->dispatcher->expects($this->at(1))->method('dispatch')->with(KernelEvents::RESPONSE, $this->anything());
+    $this->dispatcher->expects($this->at(2))->method('dispatch')->with(KernelEvents::FINISH_REQUEST, $this->anything());
+
+    $this->requestStack->expects($this->once())->method('pop');
+
+    $this->httpKernel->handle($this->request);
+  }
+
+  public function testUnableToFindController() {
+    try {
+      $this->requestStack->expects($this->once())->method('push');
+
+      $this->dispatcher->expects($this->exactly(2))->method('dispatch')->withConsecutive(
+	  array(KernelEvents::REQUEST, $this->anything()),
+	  array(KernelEvents::FINISH_REQUEST, $this->anything())
+      );
+
+      $this->router->expects($this->once())->method('lookup');
+
+      $this->resolver->expects($this->once())->method('getController')->willReturn(false);
+      
+      $this->httpKernel->handle($this->request, HttpKernelInterface::MASTER_REQUEST, false);
+      $this->assertTrue(false);
+    } catch(NotFoundHttpException $e) {
+      // DO NOTHING
+    }
+  }
+
+  public function testExceptionIfResponseIsNotAResponse() {
+    try {
+      $this->requestStack->expects($this->once())->method('push');
+
+      $this->dispatcher->expects($this->exactly(4))->method('dispatch')->withConsecutive(
+	  array(KernelEvents::REQUEST, $this->anything()),
+	  array(KernelEvents::CONTROLLER, $this->anything()),
+	  array(KernelEvents::VIEW, $this->anything()),
+	  array(KernelEvents::FINISH_REQUEST, $this->anything())
+      );
+
+      $this->router->expects($this->once())->method('lookup');
+
+      $this->resolver->expects($this->once())->method('getController')->willReturn(function() { return ""; });
+      $this->resolver->expects($this->once())->method('getArguments')->willReturn($this->arguments);
+      
+      $this->requestStack->expects($this->once())->method('pop');
+
+      $this->httpKernel->handle($this->request, HttpKernelInterface::MASTER_REQUEST, false);
+      $this->assertTrue(false);
+    } catch(LogicException $e) {
+      // DO NOTHING
+    }
+  }
+
+  public function testExceptionIfResponseIsNull() {
+    try {
+      $this->requestStack->expects($this->once())->method('push');
+
+      $this->dispatcher->expects($this->exactly(4))->method('dispatch')->withConsecutive(
+	  array(KernelEvents::REQUEST, $this->anything()),
+	  array(KernelEvents::CONTROLLER, $this->anything()),
+	  array(KernelEvents::VIEW, $this->anything()),
+	  array(KernelEvents::FINISH_REQUEST, $this->anything())
+      );
+
+      $this->router->expects($this->once())->method('lookup');
+
+      $this->resolver->expects($this->once())->method('getController')->willReturn(function() { });
+      $this->resolver->expects($this->once())->method('getArguments')->willReturn($this->arguments);
+      
+      $this->requestStack->expects($this->once())->method('pop');
+
+      $this->httpKernel->handle($this->request, HttpKernelInterface::MASTER_REQUEST, false);
+      $this->assertTrue(false);
+    } catch(LogicException $e) {
+      // DO NOTHING
+    }
   }
 
   public function testHandleException() {
