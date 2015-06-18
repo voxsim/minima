e35a2b7638cf680c51cbe6c824357f08d26593a6
Simon Vocella <voxsim@Simons-MacBook-Pro.local>
2015-06-18 11:39:15 +0200

    introduced NullHttpKernel in ResponsePreparerInterface for compatibility with HttpKernel

--- src/response/ResponsePreparer.php
+++ src/response/ResponsePreparer.php
@@ -1,10 +1,10 @@
 <?php namespace Minima\Response;
 
+use Minima\Kernel\NullHttpKernel;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,10 +17,10 @@ class ResponsePreparer implements ResponsePreparerInterface
     $this->dispatcher = $dispatcher;
   }
 
-  public function validateAndPrepare($response, Request $request, $type, HttpKernelInterface $httpKernel)
+  public function validateAndPrepare($response, Request $request, $type)
   {
     if (!$response instanceof Response) {
-	$event = new GetResponseForControllerResultEvent($httpKernel, $request, $type, $response);
+	$event = new GetResponseForControllerResultEvent(new NullHttpKernel(), $request, $type, $response);
 	$this->dispatcher->dispatch(KernelEvents::VIEW, $event);
 
 	if ($event->hasResponse()) {
@@ -38,12 +38,12 @@ class ResponsePreparer implements ResponsePreparerInterface
 	}
     }
 
-    return $this->prepare($response, $request, $type, $httpKernel);
+    return $this->prepare($response, $request, $type);
   }
   
-  public function prepare(Response $response, Request $request, $type, HttpKernelInterface $httpKernel)
+  public function prepare(Response $response, Request $request, $type)
   {
-      $event = new FilterResponseEvent($httpKernel, $request, $type, $response);
+      $event = new FilterResponseEvent(new NullHttpKernel(), $request, $type, $response);
 
       $this->dispatcher->dispatch(KernelEvents::RESPONSE, $event);
 
--- src/response/ResponsePreparerInterface.php
+++ src/response/ResponsePreparerInterface.php
@@ -1,10 +1,9 @@
 <?php namespace Minima\Response;
 
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 interface ResponsePreparerInterface {
-  public function validateAndPrepare($response, Request $request, $type, HttpKernelInterface $httpKernel);
-  public function prepare(Response $response, Request $request, $type, HttpKernelInterface $httpKernel);
+  public function validateAndPrepare($response, Request $request, $type);
+  public function prepare(Response $response, Request $request, $type);
 }
