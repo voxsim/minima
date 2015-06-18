7fae8e9d27ab33420124e3ae7496c6797785303a
Simon Vocella <voxsim@Simons-MacBook-Pro.local>
2015-06-18 15:08:11 +0200

    added unit test for StringToResponseListener

---
+++ unit-tests/Listener/StringToResponseListenerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Minima\Listener\StringToResponseListener;
+use Symfony\Component\HttpFoundation\Response;
+
+class StringToResponseListenerTest extends \PHPUnit_Framework_TestCase {
+
+  public function testOnKernelViewWhenResponseIsNotValid()
+  {
+    $listener = new StringToResponseListener();
+
+    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent')->disableOriginalConstructor()->getMock();
+
+    $event->expects($this->once())->method('getControllerResult')->willReturn('string-response');
+    $event->expects($this->once())->method('setResponse');
+
+    $listener->onKernelView($event);
+  }
+
+  public function testOnKernelView()
+  {
+    $listener = new StringToResponseListener();
+
+    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent')->disableOriginalConstructor()->getMock();
+
+    $event->expects($this->once())->method('getControllerResult')->willReturn(new Response());
+    $event->expects($this->never())->method('setResponse');
+
+    $listener->onKernelView($event);
+  }
+}
