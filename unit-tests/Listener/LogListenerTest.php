e622e91a14a0c7c4adbe6011759dd08af41ddf64
Simon Vocella <voxsim@Simons-MacBook-Pro.local>
2015-06-18 16:16:01 +0200

    clean tests for LogListener

--- unit-tests/Listener/LogListenerTest.php
+++ unit-tests/Listener/LogListenerTest.php
@@ -9,114 +9,100 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class LogListenerTest extends \PHPUnit_Framework_TestCase {
 
+  public function __construct()
+  {
+    $this->logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
+    $this->listener = new LogListener($this->logger);
+
+    $this->requestEvent = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseEvent')->disableOriginalConstructor()->getMock();
+    $this->responseEvent = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\FilterResponseEvent')->disableOriginalConstructor()->getMock();
+    $this->exceptionEvent = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent')->disableOriginalConstructor()->getMock();
+  }
+
   public function testOnKernelRequest()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $request = Request::create('/route', 'GET');
 
-    $event->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::MASTER_REQUEST);
-    $event->expects($this->once())->method('getRequest')->willReturn($request);
-    $logger->expects($this->once())->method('info')->with('> GET /route');
+    $this->requestEvent->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::MASTER_REQUEST);
+    $this->requestEvent->expects($this->once())->method('getRequest')->willReturn($request);
+    $this->logger->expects($this->once())->method('info')->with('> GET /route');
 
-    $listener->onKernelRequest($event);
+    $this->listener->onKernelRequest($this->requestEvent);
   }
 
   public function testOnKernelRequestForNonMasterRequest()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $request = Request::create('/route', 'GET');
 
-    $event->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::SUB_REQUEST);
-    $event->expects($this->never())->method('getRequest')->willReturn($request);
-    $logger->expects($this->never())->method('info')->with('> GET /route');
+    $this->requestEvent->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::SUB_REQUEST);
+    $this->requestEvent->expects($this->never())->method('getRequest')->willReturn($request);
+    $this->logger->expects($this->never())->method('info')->with('> GET /route');
 
-    $listener->onKernelRequest($event);
+    $this->listener->onKernelRequest($this->requestEvent);
   }
 
   public function testOnKernelResponse()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\FilterResponseEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $response = new Response();
     $response->setStatusCode(404);
 
-    $event->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::MASTER_REQUEST);
-    $event->expects($this->once())->method('getResponse')->willReturn($response);
-    $logger->expects($this->once())->method('info')->with('< 404');
+    $this->responseEvent->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::MASTER_REQUEST);
+    $this->responseEvent->expects($this->once())->method('getResponse')->willReturn($response);
+    $this->logger->expects($this->once())->method('info')->with('< 404');
 
-    $listener->onKernelResponse($event);
+    $this->listener->onKernelResponse($this->responseEvent);
   }
 
   public function testOnKernelResponseForNonMasterRequest()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\FilterResponseEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $response = new Response();
     $response->setStatusCode(404);
 
-    $event->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::SUB_REQUEST);
-    $event->expects($this->never())->method('getResponse');
-    $logger->expects($this->never())->method('info');
+    $this->responseEvent->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::SUB_REQUEST);
+    $this->responseEvent->expects($this->never())->method('getResponse');
+    $this->logger->expects($this->never())->method('info');
 
-    $listener->onKernelResponse($event);
+    $this->listener->onKernelResponse($this->responseEvent);
   }
 
   public function testOnKernelResponseForRedirectResponse()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\FilterResponseEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $response = new RedirectResponse('www.target.url', 302);
 
-    $event->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::MASTER_REQUEST);
-    $event->expects($this->once())->method('getResponse')->willReturn($response);
-    $logger->expects($this->once())->method('info')->with('< 302 www.target.url');
+    $this->responseEvent->expects($this->once())->method('getRequestType')->willReturn(HttpKernelInterface::MASTER_REQUEST);
+    $this->responseEvent->expects($this->once())->method('getResponse')->willReturn($response);
+    $this->logger->expects($this->once())->method('info')->with('< 302 www.target.url');
 
-    $listener->onKernelResponse($event);
+    $this->listener->onKernelResponse($this->responseEvent);
   }
 
   public function testOnKernelException()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $exception = new \Exception();
 
-    $event->expects($this->once())->method('getException')->willReturn($exception);
-    $logger->expects($this->once())->method('critical')->with($this->anything(), array('exception' =>$exception));
+    $this->exceptionEvent->expects($this->once())->method('getException')->willReturn($exception);
+    $this->logger->expects($this->once())->method('critical')->with($this->anything(), array('exception' =>$exception));
 
-    $listener->onKernelException($event);
+    $this->listener->onKernelException($this->exceptionEvent);
   }
 
   public function testOnKernelExceptionForHttpException()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $exception = new HttpException(404);
 
-    $event->expects($this->once())->method('getException')->willReturn($exception);
-    $logger->expects($this->once())->method('error')->with($this->anything(), array('exception' =>$exception));
+    $this->exceptionEvent->expects($this->once())->method('getException')->willReturn($exception);
+    $this->logger->expects($this->once())->method('error')->with($this->anything(), array('exception' =>$exception));
 
-    $listener->onKernelException($event);
+    $this->listener->onKernelException($this->exceptionEvent);
   }
 
   public function testOnKernelExceptionForHttpExceptionGreaterThan500()
   {
-    $logger = $this->getMockBuilder('Psr\Log\LoggerInterface')->getMock();
-    $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent')->disableOriginalConstructor()->getMock();
-    $listener = new LogListener($logger);
     $exception = new HttpException(500);
 
-    $event->expects($this->once())->method('getException')->willReturn($exception);
-    $logger->expects($this->once())->method('critical')->with($this->anything(), array('exception' =>$exception));
+    $this->exceptionEvent->expects($this->once())->method('getException')->willReturn($exception);
+    $this->logger->expects($this->once())->method('critical')->with($this->anything(), array('exception' =>$exception));
 
-    $listener->onKernelException($event);
+    $this->listener->onKernelException($this->exceptionEvent);
   }
 }
